1f51dae67efe6faa86c7d6e8c9138a2d473b7ae2
stanley <stanleygomesdasilva@gmail.com>
2025-06-04 17:47:35 -0300

    chore: exemplo de doc para endpoint swagger

--- src/routes/ping.route.ts
+++ src/routes/ping.route.ts
@@ -1,8 +1,22 @@
-// filepath: /home/stanley/Downloads/projects/aibank/garden-of-eden/src/routes/ping.route.ts
 import { FastifyInstance } from 'fastify'
 
 export default async function pingRoutes(server: FastifyInstance) {
-  server.get('/ping', async (request, reply) => {
-    return 'pong!'
+  server.get('/ping', {
+    schema: {
+      description: 'Ping route',
+      tags: ['Health'],
+      summary: 'Retorna pong!',
+      response: {
+        200: {
+          description: 'Resposta de sucesso',
+          type: 'object',
+          properties: {
+            message: { type: 'string', example: 'pong!' }
+          }
+        }
+      }
+    }
+  }, async (request, reply) => {
+    return { message: 'pong!' }
   })
 }
