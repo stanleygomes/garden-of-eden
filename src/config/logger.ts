1501d69e475803a894daee67d2e8512d3b9a5b3d
stanley <stanleygomesdasilva@gmail.com>
2025-06-20 09:58:17 -0300

    chore: iniciar projeto do zero

--- src/config/logger.ts
+++
@@ -1,9 +0,0 @@
-import pino, { Logger } from 'pino';
-
-export const logger: Logger = pino({
-  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-  transport:
-    process.env.NODE_ENV !== 'production'
-      ? { target: 'pino-pretty' }
-      : undefined,
-});
--- src/router.ts
+++
@@ -1,5 +0,0 @@
-import pingRoutes from './routes/ping.route'
-
-export default [
-  pingRoutes,
-]
